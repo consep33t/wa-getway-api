045d1dc62513bbff445760473ecf64cd30e61ea8
Ageng Prayoga <ageng_ojt@solvera.id>
2026-07-19 02:18:18 +0700

    fix: Remove remote webVersionCache to prevent silent message dropping by WA server

--- src/services/whatsapp.js
+++ src/services/whatsapp.js
@@ -13,10 +13,6 @@ const initializeClient = () => {
     puppeteer: {
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
       protocolTimeout: 300000,
-    },
-    webVersionCache: {
-      type: 'remote',
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
     }
   });
 
