2443c88b1ec186cb0881750fba427f9684fe5050
Ageng Prayoga <ageng_ojt@solvera.id>
2026-07-25 08:47:05 +0700

    fix(puppeteer): add webVersionCache and args to fix Execution context was destroyed error

--- src/services/whatsapp.js
+++ src/services/whatsapp.js
@@ -66,8 +66,21 @@ const initializeClient = () => {
 
   client = new Client({
     authStrategy: new LocalAuth({ dataPath: 'sessions' }),
+    webVersionCache: {
+      type: 'remote',
+      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+    },
     puppeteer: {
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      args: [
+        '--no-sandbox', 
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // helps fix context issues in docker
+        '--disable-gpu'
+      ],
       protocolTimeout: 300000,
     }
   });
